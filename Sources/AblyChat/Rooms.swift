8b41045adc5ae04c51a56190fd300ebbd72f869c
Lawrence Forooghian <lawrence@forooghian.com>
2024-12-02 11:48:50 -0300

    wip
    tests to come after; just want to gauge effort
    
    TODO logging
    
    wip remove releaseTasks and unify state
    
    and also now am adding the CHA-RC1g4 behaviour of failing existing room
    gets

--- Sources/AblyChat/Rooms.swift
+++ Sources/AblyChat/Rooms.swift
@@ -21,8 +21,48 @@ internal actor DefaultRooms<RoomFactory: AblyChat.RoomFactory>: Rooms {
     private let logger: InternalLogger
     private let roomFactory: RoomFactory
 
+    private enum RoomState {
+        /// There is no room map entry for this room ID, but a CHA-RC1g release operation is in progress.
+        case releaseOperationInProgress(releaseTask: Task<Void, Never>)
+
+        /// There is a room map entry for this room ID.
+        case roomMapEntry(RoomMapEntry)
+    }
+
+    private enum RoomMapEntry {
+        /// The room has been requested, but is awaiting the completion of a CHA-RC1g release operation.
+        case requestAwaitingRelease(
+            releaseTask: Task<Void, Never>,
+            requestedOptions: RoomOptions,
+            creationTask: Task<RoomFactory.Room, Error>,
+            failCreationTask: @Sendable (Error) -> Void
+        )
+
+        /// The room has been created.
+        case created(room: RoomFactory.Room)
+
+        var roomOptions: RoomOptions {
+            switch self {
+            case let .requestAwaitingRelease(_, requestedOptions: options, _, _):
+                options
+            case let .created(room):
+                room.options
+            }
+        }
+
+        func waitForRoom() async throws -> RoomFactory.Room {
+            switch self {
+            case let .requestAwaitingRelease(_, _, creationTask: creationTask, _):
+                try await creationTask.value
+            case let .created(room):
+                room
+            }
+        }
+    }
+
+    // TODO: update comment
     /// The set of rooms, keyed by room ID.
-    private var rooms: [String: RoomFactory.Room] = [:]
+    private var roomStates: [String: RoomState] = [:]
 
     internal init(realtime: RealtimeClient, clientOptions: ClientOptions, logger: InternalLogger, roomFactory: RoomFactory) {
         self.realtime = realtime
@@ -33,39 +73,115 @@ internal actor DefaultRooms<RoomFactory: AblyChat.RoomFactory>: Rooms {
     }
 
     internal func get(roomID: String, options: RoomOptions) async throws -> any Room {
-        // CHA-RC1b
-        if let existingRoom = rooms[roomID] {
-            // CHA-RC1c
-            if existingRoom.options != options {
-                throw ARTErrorInfo(
-                    chatError: .inconsistentRoomOptions(requested: options, existing: existingRoom.options)
-                )
-            }
+        if let existingRoomState = roomStates[roomID] {
+            switch existingRoomState {
+            // TODO: when testing this, test all cases of RoomMapEntry
+            case let .roomMapEntry(existingRoomMapEntry):
+                // CHA-RC1f1
+                if existingRoomMapEntry.roomOptions != options {
+                    throw ARTErrorInfo(
+                        chatError: .inconsistentRoomOptions(requested: options, existing: existingRoomMapEntry.roomOptions)
+                    )
+                }
 
-            return existingRoom
+                // TODO: which of CHA-RC1f1 OR cha-rc1F2 takes priority here? i.e. do we now need to consider whether there's a release in progress? I guess that's what's happening anyway since this waits for something that's waiting for a release
+
+                // CHA-RC1f2
+                return try await existingRoomMapEntry.waitForRoom()
+            case let .releaseOperationInProgress(releaseTask: releaseTask):
+                // TODO: tidy this stuff up
+                let (stream, continuation) = AsyncThrowingStream.makeStream(of: Void.self, throwing: Error.self)
+
+                let failCreationTask = { @Sendable (error: Error) in
+                    continuation.finish(throwing: error)
+                }
+
+                let creationTask = Task {
+                    try await withThrowingTaskGroup(of: Void.self) { group in
+                        group.addTask {
+                            try await stream.first { _ in true }
+                        }
+
+                        group.addTask {
+                            // TODO: how to make this exit if the task is cancelled? the detached is to not propagate cancellation, but that won't make cancellation break us out of this `await`. Hmm
+                            await Task.detached {
+                                // CHA-RC1f4
+                                await releaseTask.value
+                            }.value
+                        }
+
+                        // This pattern for waiting for the first of multiple tasks is taken from here
+                        // https://forums.swift.org/t/accept-the-first-task-to-complete/54386
+                        defer { group.cancelAll() }
+                        // TODO: how do we guarantee we’ll get the right error?
+                        try await group.next()
+                    }
+
+                    return try await createRoom(roomID: roomID, options: options)
+                }
+                roomStates[roomID] = .roomMapEntry(
+                    .requestAwaitingRelease(
+                        releaseTask: releaseTask,
+                        requestedOptions: options,
+                        creationTask: creationTask,
+                        failCreationTask: failCreationTask
+                    )
+                )
+                return try await creationTask.value
+            }
         }
 
+        // CHA-RC1f3
+        return try await createRoom(roomID: roomID, options: options)
+    }
+
+    private func createRoom(roomID: String, options: RoomOptions) async throws -> RoomFactory.Room {
         let room = try await roomFactory.createRoom(realtime: realtime, chatAPI: chatAPI, roomID: roomID, options: options, logger: logger)
-        rooms[roomID] = room
+        roomStates[roomID] = .roomMapEntry(.created(room: room))
         return room
     }
 
     #if DEBUG
+        // TODO: what is this? check it still makes sense in our tests
         internal func testsOnly_hasExistingRoomWithID(_ roomID: String) -> Bool {
-            rooms[roomID] != nil
+            roomStates[roomID] != nil
         }
     #endif
 
     internal func release(roomID: String) async throws {
-        guard let room = rooms[roomID] else {
-            // TODO: what to do here? (https://github.com/ably/specification/pull/200/files#r1837154563) — Andy replied that it’s a no-op but that this is going to be specified in an upcoming PR when we make room-getting async
+        guard let roomState = roomStates[roomID] else {
+            // CHA-RC1g2 (no-op)
             return
         }
 
-        // CHA-RC1d
-        rooms.removeValue(forKey: roomID)
+        switch roomState {
+        case let .releaseOperationInProgress(releaseTask):
+            // CHA-RC1g3
+            await releaseTask.value
+        case let .roomMapEntry(
+            .requestAwaitingRelease(
+                releaseTask: releaseTask,
+                _,
+                _,
+                failCreationTask: failCreationTask
+            )
+        ):
+            // CHA-RC1g4
+            failCreationTask(ARTErrorInfo(chatError: .roomReleasedBeforeOperationCompleted))
+            await releaseTask.value
+        case let .roomMapEntry(.created(room: room)):
+            // TODO: this (and creationTask) is a different approach to that taken in the lifecycle manager for waiting; maybe this is neater even though you could argue there's an unnecessary task here; also you don't have to worry about task priorities
+            // TODO: explain that we expect releaseTask to happen afterwards, always, so that state changes happen in correct order
+            let releaseTask = Task {
+                // Clear the `.releaseOperationInProgress` state (written in a `defer` in case `room.release()` becomes throwing in the future)
+                defer { roomStates.removeValue(forKey: roomID) }
+                await room.release()
+            }
 
-        // CHA-RL1e
-        await room.release()
+            // This also achieves CHA-RC1g5 (remove room from room map)
+            roomStates[roomID] = .releaseOperationInProgress(releaseTask: releaseTask)
+
+            await releaseTask.value
+        }
     }
 }
