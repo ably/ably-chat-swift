9c7edad70748f0106a064b4b3e668454f401574d
Marat Al <m.alekperov@gmail.com>
2024-12-09 00:37:09 +0100

    Add docstring from JS Discontinuities
    Taken from JS repo at 69ea478, not edited at all.

--- Sources/AblyChat/EmitsDiscontinuities.swift
+++ Sources/AblyChat/EmitsDiscontinuities.swift
@@ -9,9 +9,18 @@ public struct DiscontinuityEvent: Sendable, Equatable {
     }
 }
 
+/**
+ * An interface to be implemented by objects that can emit discontinuities to listeners.
+ */
 public protocol EmitsDiscontinuities {
+    /**
+     * Register a listener to be called when a discontinuity is detected.
+     * @param listener The listener to be called when a discontinuity is detected.
+     * @returns A response that allows control of the subscription.
+     */
     func onDiscontinuity(bufferingPolicy: BufferingPolicy) async -> Subscription<DiscontinuityEvent>
-    /// Same as calling ``onDiscontinuity(bufferingPolicy:)`` with ``BufferingPolicy.unbounded``.
+
+    /// Same as calling ``onDiscontinuity(bufferingPolicy:)`` with ``BufferingPolicy/unbounded``.
     ///
     /// The `EmitsDiscontinuities` protocol provides a default implementation of this method.
     func onDiscontinuity() async -> Subscription<DiscontinuityEvent>
