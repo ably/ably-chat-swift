909a67f9b4c699c5c6b73c6bfba97b44b80380f4
Marat Al <m.alekperov@gmail.com>
2024-12-09 00:38:27 +0100

    Add docstring from JS logging
    Taken from JS repo at 69ea478, not edited at all.

--- Sources/AblyChat/Logging.swift
+++ Sources/AblyChat/Logging.swift
@@ -3,9 +3,18 @@ import os
 public typealias LogContext = [String: any Sendable]
 
 public protocol LogHandler: AnyObject, Sendable {
+    /**
+     * A function that can be used to handle log messages.
+     * @param message The message to log.
+     * @param level The log level of the message.
+     * @param context The context of the log message as key-value pairs.
+     */
     func log(message: String, level: LogLevel, context: LogContext?)
 }
 
+/**
+ * Represents the different levels of logging that can be used.
+ */
 public enum LogLevel: Sendable, Comparable {
     case trace
     case debug
