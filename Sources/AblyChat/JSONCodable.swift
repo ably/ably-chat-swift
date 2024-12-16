8b8a084083ea07e308648157f6b4f3e699090bd3
Lawrence Forooghian <lawrence@forooghian.com>
2024-12-16 16:04:33 -0300

    Add some helper methods for implementing JSONCodable
    Will use these in an upcoming commit.

--- Sources/AblyChat/JSONCodable.swift
+++ Sources/AblyChat/JSONCodable.swift
@@ -25,6 +25,8 @@ internal protocol JSONObjectDecodable: JSONDecodable {
 
 internal enum JSONValueDecodingError: Error {
     case valueIsNotObject
+    case noValueForKey(String)
+    case wrongTypeForKey(String, actualValue: JSONValue)
 }
 
 // Default implementation of `JSONDecodable` conformance for `JSONObjectDecodable`
@@ -39,3 +41,190 @@ internal extension JSONObjectDecodable {
 }
 
 internal typealias JSONObjectCodable = JSONObjectDecodable & JSONObjectEncodable
+
+// MARK: - Extracting values from a dictionary
+
+/// This extension adds some helper methods for extracting values from a dictionary of `JSONValue` values; you may find them helpful when implementing `JSONCodable`.
+internal extension [String: JSONValue] {
+    /// If this dictionary contains a value for `key`, and this value has case `object`, this returns the associated value.
+    ///
+    /// - Throws:
+    ///   - `JSONValueDecodingError.noValueForKey` if the key is absent
+    ///   - `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `object`
+    func objectValueForKey(_ key: String) throws -> [String: JSONValue] {
+        guard let value = self[key] else {
+            throw JSONValueDecodingError.noValueForKey(key)
+        }
+
+        guard case let .object(objectValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return objectValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `object`, this returns the associated value. If this dictionary does not contain a value for `key`, or if the value for key has case `null`, it returns `nil`.
+    ///
+    /// - Throws: `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `object` or `null`
+    func optionalObjectValueForKey(_ key: String) throws -> [String: JSONValue]? {
+        guard let value = self[key] else {
+            return nil
+        }
+
+        if case .null = value {
+            return nil
+        }
+
+        guard case let .object(objectValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return objectValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `array`, this returns the associated value.
+    ///
+    /// - Throws:
+    ///   - `JSONValueDecodingError.noValueForKey` if the key is absent
+    ///   - `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `array`
+    func arrayValueForKey(_ key: String) throws -> [JSONValue] {
+        guard let value = self[key] else {
+            throw JSONValueDecodingError.noValueForKey(key)
+        }
+
+        guard case let .array(arrayValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return arrayValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `array`, this returns the associated value. If this dictionary does not contain a value for `key`, or if the value for key has case `null`, it returns `nil`.
+    ///
+    /// - Throws: `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `array` or `null`
+    func optionalArrayValueForKey(_ key: String) throws -> [JSONValue]? {
+        guard let value = self[key] else {
+            return nil
+        }
+
+        if case .null = value {
+            return nil
+        }
+
+        guard case let .array(arrayValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return arrayValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `string`, this returns the associated value.
+    ///
+    /// - Throws:
+    ///   - `JSONValueDecodingError.noValueForKey` if the key is absent
+    ///   - `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `string`
+    func stringValueForKey(_ key: String) throws -> String {
+        guard let value = self[key] else {
+            throw JSONValueDecodingError.noValueForKey(key)
+        }
+
+        guard case let .string(stringValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return stringValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `string`, this returns the associated value. If this dictionary does not contain a value for `key`, or if the value for key has case `null`, it returns `nil`.
+    ///
+    /// - Throws: `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `string` or `null`
+    func optionalStringValueForKey(_ key: String) throws -> String? {
+        guard let value = self[key] else {
+            return nil
+        }
+
+        if case .null = value {
+            return nil
+        }
+
+        guard case let .string(stringValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return stringValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `number`, this returns the associated value.
+    ///
+    /// - Throws:
+    ///   - `JSONValueDecodingError.noValueForKey` if the key is absent
+    ///   - `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `number`
+    func numberValueForKey(_ key: String) throws -> Double {
+        guard let value = self[key] else {
+            throw JSONValueDecodingError.noValueForKey(key)
+        }
+
+        guard case let .number(numberValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return numberValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `number`, this returns the associated value. If this dictionary does not contain a value for `key`, or if the value for key has case `null`, it returns `nil`.
+    ///
+    /// - Throws: `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `number` or `null`
+    func optionalNumberValueForKey(_ key: String) throws -> Double? {
+        guard let value = self[key] else {
+            return nil
+        }
+
+        if case .null = value {
+            return nil
+        }
+
+        guard case let .number(numberValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return numberValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `bool`, this returns the associated value.
+    ///
+    /// - Throws:
+    ///   - `JSONValueDecodingError.noValueForKey` if the key is absent
+    ///   - `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `bool`
+    func boolValueForKey(_ key: String) throws -> Bool {
+        guard let value = self[key] else {
+            throw JSONValueDecodingError.noValueForKey(key)
+        }
+
+        guard case let .bool(boolValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return boolValue
+    }
+
+    /// If this dictionary contains a value for `key`, and this value has case `bool`, this returns the associated value.
+    ///
+    /// - Throws:
+    ///   - `JSONValueDecodingError.noValueForKey` if the key is absent
+    ///   - `JSONValueDecodingError.wrongTypeForKey` if the value does not have case `bool`
+    func optionalBoolValueForKey(_ key: String) throws -> Bool? {
+        guard let value = self[key] else {
+            return nil
+        }
+
+        if case .null = value {
+            return nil
+        }
+
+        guard case let .bool(boolValue) = value else {
+            throw JSONValueDecodingError.wrongTypeForKey(key, actualValue: value)
+        }
+
+        return boolValue
+    }
+}
