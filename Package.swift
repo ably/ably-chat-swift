44309f52f0d1355e177f8789f1f975adce3f67fe
Lawrence Forooghian <lawrence@forooghian.com>
2025-01-14 17:07:39 -0300

    Bump ably-cocoa version
    So that we can use the wrapper SDK proxy functionality.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/ably/ably-cocoa",
-            from: "1.2.37"
+            from: "1.2.38"
         ),
         .package(
             url: "https://github.com/apple/swift-argument-parser",
