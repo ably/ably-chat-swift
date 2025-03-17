39b431365aa8d83d0bde38bf8355c7ba86a7d703
Marat Al <m.alekperov@gmail.com>
2025-03-17 13:30:53 +0100

    Add presence tests without using `DefaultRoomLifecycleManager`.
    This reverts 5a55c69

--- Tests/AblyChatTests/Mocks/MockRealtimePresence.swift
+++ Tests/AblyChatTests/Mocks/MockRealtimePresence.swift
@@ -1,17 +1,25 @@
 import Ably
 import AblyChat
 
-final class MockRealtimePresence: RealtimePresenceProtocol {
-    var syncComplete: Bool {
-        fatalError("Not implemented")
+final class MockRealtimePresence: NSObject, RealtimePresenceProtocol {
+    let callRecorder = MockMethodCallRecorder()
+
+    let syncComplete: Bool
+
+    init(syncComplete: Bool = true) {
+        self.syncComplete = syncComplete
     }
 
-    func get(_: @escaping ARTPresenceMessagesCallback) {
-        fatalError("Not implemented")
+    func get(_ callback: @escaping ARTPresenceMessagesCallback) {
+        callRecorder.addRecord(signature: "\(#selector(Self.get(_:)))",
+                               arguments: [:])
+        callback([], nil)
     }
 
-    func get(_: ARTRealtimePresenceQuery, callback _: @escaping ARTPresenceMessagesCallback) {
-        fatalError("Not implemented")
+    func get(_ query: ARTRealtimePresenceQuery, callback: @escaping ARTPresenceMessagesCallback) {
+        callRecorder.addRecord(signature: "\(#selector(Self.get(_:callback:)))",
+                               arguments: ["query": "\(query)"])
+        callback([], nil)
     }
 
     func enter(_: Any?) {
@@ -26,24 +34,30 @@ final class MockRealtimePresence: RealtimePresenceProtocol {
         fatalError("Not implemented")
     }
 
-    func update(_: Any?, callback _: ARTCallback? = nil) {
-        fatalError("Not implemented")
+    func update(_ data: Any?, callback: ARTCallback?) {
+        callRecorder.addRecord(signature: "\(#selector(Self.update(_:callback:)))",
+                               arguments: ["data": data as Any])
+        callback?(nil)
     }
 
     func leave(_: Any?) {
         fatalError("Not implemented")
     }
 
-    func leave(_: Any?, callback _: ARTCallback? = nil) {
-        fatalError("Not implemented")
+    func leave(_ data: Any?, callback: ARTCallback?) {
+        callRecorder.addRecord(signature: "\(#selector(Self.leave(_:callback:)))",
+                               arguments: ["data": data as Any])
+        callback?(nil)
     }
 
     func enterClient(_: String, data _: Any?) {
         fatalError("Not implemented")
     }
 
-    func enterClient(_: String, data _: Any?, callback _: ARTCallback? = nil) {
-        fatalError("Not implemented")
+    func enterClient(_ name: String, data: Any?, callback: ARTCallback? = nil) {
+        callRecorder.addRecord(signature: "\(#selector(Self.enterClient(_:data:callback:)))",
+                               arguments: ["name": name, "data": data as Any])
+        callback?(nil)
     }
 
     func updateClient(_: String, data _: Any?) {
@@ -71,7 +85,7 @@ final class MockRealtimePresence: RealtimePresenceProtocol {
     }
 
     func subscribe(_: ARTPresenceAction, callback _: @escaping ARTPresenceMessageCallback) -> ARTEventListener? {
-        fatalError("Not implemented")
+        ARTEventListener()
     }
 
     func subscribe(_: ARTPresenceAction, onAttach _: ARTCallback?, callback _: @escaping ARTPresenceMessageCallback) -> ARTEventListener? {
@@ -83,7 +97,7 @@ final class MockRealtimePresence: RealtimePresenceProtocol {
     }
 
     func unsubscribe(_: ARTEventListener) {
-        fatalError("Not implemented")
+        // no-op since it's called automatically
     }
 
     func unsubscribe(_: ARTPresenceAction, listener _: ARTEventListener) {
@@ -96,5 +110,11 @@ final class MockRealtimePresence: RealtimePresenceProtocol {
 
     func history(_: ARTRealtimeHistoryQuery?, callback _: @escaping ARTPaginatedPresenceCallback) throws {
         fatalError("Not implemented")
+    }
+}
+
+extension ARTRealtimePresenceQuery {
+    override open var description: String {
+        "clientId=\(clientId!)"
     }
 }
