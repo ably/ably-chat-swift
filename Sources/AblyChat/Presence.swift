00c4149ef9bb90a23df6665535cd9e570542d1f1
Lawrence Forooghian <lawrence@forooghian.com>
2025-04-15 11:41:31 -0300

    Keep the PresenceParams type
    I originally created this as a way of getting around some compiler
    concurrency checking warnings when we used ably-cocoa's
    ARTRealtimePresenceQuery type. Now that we've switched to @MainActor in
    fc83fc1, we could switch back to using the ably-cocoa type.
    
    However, I think it's best that we keep this Swift type, because I think
    it makes more sense to use a value type instead of the mutable reference
    type used by ably-cocoa.
    
    Documentation is adapted from core SDK canonical docstrings in spec repo.
    
    Resolves #64.

--- Sources/AblyChat/Presence.swift
+++ Sources/AblyChat/Presence.swift
@@ -268,16 +268,15 @@ public struct PresenceEvent: Sendable {
     }
 }
 
-// This is a Sendable equivalent of ably-cocoa’s ARTRealtimePresenceQuery type.
-//
-// Originally, ``Presence/get(params:)`` accepted an ARTRealtimePresenceQuery object, but I’ve changed it to accept this type, because else when you try and write an actor that implements ``Presence``, you get a compiler error like "Non-sendable type 'ARTRealtimePresenceQuery' in parameter of the protocol requirement satisfied by actor-isolated instance method 'get(params:)' cannot cross actor boundary; this is an error in the Swift 6 language mode".
-//
-// Now, based on my limited understanding, you _should_ be able to send non-Sendable values from one isolation domain to another (the purpose of the "region-based isolation" and "`sending` parameters" features added in Swift 6), but to get this to work I had to mark ``Presence`` as requiring conformance to the `Actor` protocol, and since I didn’t understand _why_ I had to do that, I didn’t want to put it in the public API.
-//
-// So, for now, let’s just accept this copy (which I don’t think is a big problem anyway); we can always revisit it with more Swift concurrency knowledge in the future. Created https://github.com/ably-labs/ably-chat-swift/issues/64 to revisit.
+/// Describes the parameters accepted by ``Presence/get(params:)``.
 public struct PresenceParams: Sendable {
+    /// Filters the array of returned presence members by a specific client using its ID.
     public var clientID: String?
+
+    /// Filters the array of returned presence members by a specific connection using its ID.
     public var connectionID: String?
+
+    /// Sets whether to wait for a full presence set synchronization between Ably and the clients on the room to complete before returning the results. Synchronization begins as soon as the room is ``RoomStatus/attached``. When set to `true` the results will be returned as soon as the sync is complete. When set to `false` the current list of members will be returned without the sync completing. The default is `true`.
     public var waitForSync = true
 
     public init(clientID: String? = nil, connectionID: String? = nil, waitForSync: Bool = true) {
