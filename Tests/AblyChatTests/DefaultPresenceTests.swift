9d404f5a050e93f32a89227da298a326de0b9f0a
Marat Al <m.alekperov@gmail.com>
2025-05-24 23:12:14 +0200

    Add specNotApplicable CHA-PR7c comment.

--- Tests/AblyChatTests/DefaultPresenceTests.swift
+++ Tests/AblyChatTests/DefaultPresenceTests.swift
@@ -485,4 +485,6 @@ struct DefaultPresenceTests {
         #expect(leaveEvent.action == .leave)
         #expect(leaveEvent.clientID == "client1")
     }
+
+    // @specNotApplicable CHA-PR7c - Untestable due to `AsyncSequence` subscription used which is removed once the object is removed from memory.
 }
