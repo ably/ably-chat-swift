de65183efd80ae9f61bec758b97b97f00b34f1c2
Lawrence Forooghian <lawrence@forooghian.com>
2025-10-22 13:34:37 -0300

    Fix hanging typing tests
    I'm still noticing an occasional hang in the integration tests, but I
    think that the majority of our test hangs were coming from the typing
    tests, so I'm re-enabling the SPM test CI step in order to be sure that
    these typing tests (which caused that CI step to hang) are fixed. Let's
    keep an eye on the integration tests.
    
    (It's good to have resolved most of the `swift test` hangs, because they
    were also limiting the utility of Claude Code, which kept hanging when
    trying to run the tests to verify its changes!)
    
    I'm also still observing the iOS unit tests taking substantially
    _longer_ to execute in CI than for other platforms. For example, the
    result of one test run (with the integration tests disabled):
    
    > ✔ Test run with 206 tests in 28 suites passed after 107.794 seconds.
    
    where for other platforms the unit test execution time is below 1 second.
    
    It looks like this is already covered by open issue #143, though.

--- Sources/AblyChat/TimerManager.swift
+++ Sources/AblyChat/TimerManager.swift
@@ -12,8 +12,19 @@ internal final class TimerManager<Clock: ClockProtocol> {
     internal func setTimer(interval: TimeInterval, handler: @escaping @MainActor () -> Void) {
         cancelTimer()
 
+        // Calculate the deadline before kicking off the asynchronous work. This is so that using TestClock's advance(by:) behaves reliably; we want to be sure that even if the Task below is scheduled by the system long after advance(by:) is called, its `sleep` call will still return in response to an advance(by:) call that causes `interval` to have elapsed on the clock relative to the time when `setTimer` was called.
+        let deadline = clock.now.advanced(byTimeInterval: interval)
+
         currentTask = Task {
-            try? await clock.sleep(for: .seconds(interval))
+            // This is for compatibility with the TestClock that we use in the tests; calling `sleep(until:)` with a deadline equal to the current time does _not_ make `sleep(until:)` return immediately; see https://github.com/pointfreeco/swift-clocks/issues/23 (it is not clear from the comments there whether or not this should be considered a misbehaviour; let's handle it either way).
+            //
+            // (You might ask "why would you call sleep(until:) with a deadline equal to the current time?", but bear in mind — per the above comment about Task scheduling — that this task might get scheduled long after time has advanced relative to the moment that setTimer was called.)
+            if clock.now >= deadline {
+                handler()
+                return
+            }
+
+            try? await clock.sleep(until: deadline)
             guard !Task.isCancelled else {
                 return
             }
