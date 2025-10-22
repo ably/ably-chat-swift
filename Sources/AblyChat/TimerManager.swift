b497fc1b5cb424363f1a3d62b75662e41da8739d
Lawrence Forooghian <lawrence@forooghian.com>
2025-10-22 11:45:24 -0300

    wip fix hanging tests

--- Sources/AblyChat/TimerManager.swift
+++ Sources/AblyChat/TimerManager.swift
@@ -12,12 +12,18 @@ internal final class TimerManager<Clock: ClockProtocol> {
     internal func setTimer(interval: TimeInterval, handler: @escaping @MainActor () -> Void) {
         cancelTimer()
 
-        currentTask = Task {
-            try? await clock.sleep(for: .seconds(interval))
-            guard !Task.isCancelled else {
-                return
+        if #available(iOS 26.0, macOS 26.0, tvOS 26.0, *) {
+            // TODO: Explain
+            currentTask = Task.immediate {
+                try? await clock.sleep(for: .seconds(interval))
+                guard !Task.isCancelled else {
+                    return
+                }
+                handler()
             }
-            handler()
+        } else {
+            fatalError("Task.immediate is required for tests")
+            // TODO: Fallback on earlier versions
         }
     }
 
